c7920755fc386ddc25e5ba985b4a569227a92fe4
idapidadh <idapidadh@yahoo.dk>
2026-09-18 12:00:31 +0200

    started desccribing branch startegy in development-guidelines

--- src/PrepMeUp.Api/Program.cs
+++ src/PrepMeUp.Api/Program.cs
@@ -1,7 +1,10 @@
 using Serilog;
 
+// Create builder object
 var builder = WebApplication.CreateBuilder(args);
 
+// Use Serilog library to log
+// Read logs, Run in terminal: dotnet run --project 
 builder.Host.UseSerilog((context, services, configuration) => configuration
     .ReadFrom.Configuration(context.Configuration)
     .ReadFrom.Services(services)
@@ -11,11 +14,12 @@ builder.Host.UseSerilog((context, services, configuration) => configuration
 // TODO: builder.Services.AddApplication();
 // TODO: builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // enable controller based routing styles
 builder.Services.AddProblemDetails();
-builder.Services.AddOpenApi();
-builder.Services.AddHealthChecks();
+builder.Services.AddOpenApi(); // generates the OpenAPI/Swagger schema describing endpoints
+builder.Services.AddHealthChecks(); // egisters the health-check feature (what powers /health)
 
+// Set up CORS (Cross-Origin Resource Sharing)
 const string ClientCorsPolicy = "clients";
 var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
 builder.Services.AddCors(options => options.AddPolicy(ClientCorsPolicy, policy => policy
@@ -23,6 +27,7 @@ builder.Services.AddCors(options => options.AddPolicy(ClientCorsPolicy, policy =
     .AllowAnyHeader()
     .AllowAnyMethod()));
 
+// Turns builder (L4) into an app
 var app = builder.Build();
 
 app.UseExceptionHandler();
